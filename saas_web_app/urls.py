62d886555be2f51d9c8056d724bba4bdf0713aed
MD. Sohel Chowdhury <91820562+ahsohel@users.noreply.github.com>
2022-02-28 13:56:05 +0600

    Update urls.py

--- saas_web_app/urls.py
+++ saas_web_app/urls.py
@@ -21,6 +21,9 @@ from django.conf.urls.static import static
 from  django.conf.urls import url
 from django.views.static import serve
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +32,4 @@ urlpatterns = [
     
     url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
