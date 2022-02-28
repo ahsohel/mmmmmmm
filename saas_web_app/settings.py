e93d4c6a6bc94a3700848e7a31d1fa79c7943fcf
MD. Sohel Chowdhury <91820562+ahsohel@users.noreply.github.com>
2022-02-28 14:42:02 +0600

    Update settings.py

--- saas_web_app/settings.py
+++ saas_web_app/settings.py
@@ -19,8 +19,8 @@ import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -160,8 +160,10 @@ USE_TZ = True
 
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static-comp')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static-comp')
+
 
 
 
