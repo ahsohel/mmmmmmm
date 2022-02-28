aee760faf410817a1ada6f41b57fbbdccf33d6a1
MD. Sohel Chowdhury <91820562+ahsohel@users.noreply.github.com>
2022-02-28 14:37:55 +0600

    Update settings.py

--- saas_web_app/settings.py
+++ saas_web_app/settings.py
@@ -158,10 +158,10 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static'),
 # ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static-comp')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-comp')
 
 
 
