4e1365f33cc2f7129e9abb59326b7915b86a2692
MD. Sohel Chowdhury <91820562+ahsohel@users.noreply.github.com>
2022-02-28 14:32:54 +0600

    Update settings.py

--- saas_web_app/settings.py
+++ saas_web_app/settings.py
@@ -19,7 +19,8 @@ import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -210,5 +211,5 @@ django_heroku.settings(locals())
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
+DEBUG_COLLECTSTATIC=1
 
