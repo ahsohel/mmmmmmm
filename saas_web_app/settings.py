3854ed249474ddbfe04da0cac5397a3ed2a2f0f4
MD. Sohel Chowdhury <91820562+ahsohel@users.noreply.github.com>
2022-02-28 13:14:33 +0600

    Update settings.py

--- saas_web_app/settings.py
+++ saas_web_app/settings.py
@@ -152,14 +152,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-comp')
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static-comp')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
